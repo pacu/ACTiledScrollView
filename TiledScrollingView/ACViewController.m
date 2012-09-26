8bbd9092817e47d05f0b70404f51ba681ed3360d
Francisco Gindre <francisco.gindre@gmail.com>
2012-09-26 17:05:11 -0300

    - removed MIT License

--- TiledScrollingView/ACViewController.m
+++ TiledScrollingView/ACViewController.m
@@ -5,28 +5,11 @@
 //  Created by Francisco Gindre on 9/15/12.
 //  Copyright (c) 2012 AppCrafter.biz. All rights reserved.
 //
-// MIT License
-//Permission is hereby granted, free of charge, to any person obtaining
-//a copy of this software and associated documentation files (the "Software"),
-//to deal in the Software without restriction, including without limitation
-//the rights to use, copy, modify, merge, publish, distribute, sublicense,
-//and/or sell copies of the Software, and to permit persons to whom the
-//Software is furnished to do so, subject to the following conditions:
-//
-//The above copyright notice and this permission notice shall be included
-//in all copies or substantial portions of the Software.
-//
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-//OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//THE SOFTWARE.
-//
+
 #import "ACViewController.h"
 #import "ACTiledScrollView.h"
 #import "ACTileView.h"
+#import <QuartzCore/QuartzCore.h>
 @interface ACViewController ()
 
 @end
@@ -41,12 +24,14 @@
     self.scrollview = [[[ACTiledScrollView alloc]initWithTileSize:tileSize height:3 width:4] autorelease];
     
     self.scrollview.frame =self.view.frame;
-    
+    [self.scrollview setBackgroundColor:[UIColor grayColor]];
     ACTileView *tile = [[[ACTileView alloc] initWithFrame:CGRectZero] autorelease];
     [tile setBackgroundColor:[UIColor redColor]];
     [tile setTileSize:tileSize];
     [tile setSizeInTiles:CGSizeMake(2, 2)];
-    
+
+    [tile.layer setBorderColor:[[UIColor blackColor] CGColor]];
+    [tile.layer setBorderWidth:2.0];
     [scrollview addTile:tile at:tileIndexFromIndex(0, 3)];
     
     for (NSUInteger idx =0; idx<15; idx++) {
@@ -54,6 +39,8 @@
         [v setBackgroundColor:[UIColor greenColor]];
         [v setTileSize:tileSize];
         [v setSizeInTiles:CGSizeMake(1, 1)];
+        [v.layer setBorderColor:[[UIColor blackColor] CGColor]];
+        [v.layer setBorderWidth:2.0];
         [self.scrollview addTile:v at:tileIndexFromIndex(0, 3)];
     }
     
